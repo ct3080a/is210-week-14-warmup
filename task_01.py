a32a55e43a50a13591282c2d911214d5a0906401
paisely65 <cunysps@gmail.com>
2016-04-26 00:59:17 -0400

    Task 1 revised.

--- task_01.py
+++ task_01.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 """The module uses arbitrary arguments."""
 
+
 import pet
 
+
 class Dog(pet.Pet):
+    """ This class is a subclass of Pet."""
     def __init__(self, has_shots=False, **kwargs):
         self.has_shots = has_shots
         pet.Pet.__init__(self, **kwargs)
