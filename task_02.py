b9fcefb087387388134d919687c544fbdfb60fa1
acodyjr <acody@me.com>
2016-05-03 20:59:47 +0000

    Added whitspace causing failure.

--- task_02.py
+++ task_02.py
@@ -19,8 +19,8 @@ def get_cost_per_item(shoplist):
             'Peach': 24, 'Beet': 1})
         {'Lime': 7.08, 'Peach': 95.76, 'Red Pear': 9.96}
     """
-    return{item:FRUIT[item] * shoplist[item] for item in shoplist.iterkeys()
-        if item in FRUIT}
+    return{item: FRUIT[item] * shoplist[item]
+           for item in shoplist.iterkeys() if item in FRUIT}
 
 
 def get_total_cost(shoplist):
