47bd0ab48717ee22337dc68d84f1e56dabdfceb9
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-05-06 01:43:43 +0000

    Added shopping list task

--- task_02.py
+++ task_02.py
@@ -19,24 +19,25 @@ def get_cost_per_item(shoplist):
         >>> get_cost_per_item({'Lime': 12, 'Red Pear': 4,
             'Peach': 24, 'Beet': 1})
         {'Lime': 7.08, 'Peach': 95.76, 'Red Pear': 9.96}
-        """
+    """
     return{item: FRUIT[item] * shoplist[item]
            for item in shoplist.iterkeys() if item in FRUIT}
 
-    def get_total_cost(shoplist):
-        """A docstring for a functiion using a dictionary for total cost.
 
-        Args:
-            shoplist(list): Key and value of fruit items from FRUIT.
+def get_total_cost(shoplist):
+    """A docstring for a functiion using a dictionary for total cost.
 
-        Returns:
-            float: Total cost of fruit items.
+    Args:
+        shoplist(list): Key and value of fruit items from FRUIT.
 
-        Examples:
+    Returns:
+        float: Total cost of fruit items.
 
-            >>> get_total_cost({'Lime': 12, 'Red Pear': 4,
-                'Peach': 24, 'Beet': 1})
-            112.80000000000001
-        """
-        return sum(itemtotal for itemtotal in
-                   get_cost_per_item(shoplist).itervalues())
+    Examples:
+
+        >>> get_total_cost({'Lime': 12, 'Red Pear': 4,
+            'Peach': 24, 'Beet': 1})
+        112.80000000000001
+    """
+    return sum(itemtotal for itemtotal in
+               get_cost_per_item(shoplist).itervalues())
